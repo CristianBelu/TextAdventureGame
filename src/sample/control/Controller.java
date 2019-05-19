f4213148b5ceb3a8888c97af57a3dda3c6160ce7
Belu <belucristianalin@gmail.com>
2019-05-19 11:50:05 +0300

    Remove collectable and refactor

--- src/sample/control/Controller.java
+++ src/sample/control/Controller.java
@@ -2,15 +2,15 @@ package sample.control;
 
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import sample.model.Door;
-import sample.model.Game;
-import sample.model.Key;
-import sample.model.Player;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import sample.model.*;
 
 public class Controller {
 
 
     private Game game;
+    private Player player;
 
     public TextField txtFieldCommand;
     public TextArea txtAreaGameOutput;
@@ -20,7 +20,7 @@ public class Controller {
         game = new Game(10);
         Key key = new Key();
         Door door = new Door(key);
-        Player player = new Player();
+        player = new Player();
 
         game.placeOnBoard(key,2, 3);
         game.placeOnBoard(door, 1, 5);
@@ -29,9 +29,11 @@ public class Controller {
 
 
         System.out.println(game.displayBoard());
-
+        txtAreaGameOutput.appendText(game.displayBoard() + "\n");
+        /*
         game.removeFromBoard(2, 3);
         System.out.println(game.displayBoard());
+
 
         game.placeOnBoard(key,2, 3);
         game.movePlayer(player,2, 3);
@@ -48,6 +50,16 @@ public class Controller {
         txtAreaGameOutput.appendText(game.displayBoard() + "\n");
         System.out.println(player.getArtifacts());
         txtAreaGameOutput.appendText(player.getArtifacts()+ "\n");
+
+         */
     }
 
+    public void processCommand(KeyEvent keyEvent) {
+        if (keyEvent.getCode().equals(KeyCode.ENTER)){
+            GameStrategy.processCommand(txtFieldCommand.getText(), game, player);
+            txtFieldCommand.clear();
+            txtAreaGameOutput.appendText(game.displayBoard() + "\n");
+        }
+
+    }
 }
